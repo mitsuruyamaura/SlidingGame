bec87b4af78451ca5efc86f55ad150474c666794
mitsuru_yamaura <komayou326@gmail.com>
2020-12-19 12:03:22 +0900

    キャラ追加。スライド操作追加。PhysicMaterial追加。

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public float speed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        //Sliding();
+    }
+
+    private void Sliding() {
+        rb.AddForce(transform.forward * speed, ForceMode.Impulse);
+    }
+
+    void Update()
+    {
+        
+    }
+}
