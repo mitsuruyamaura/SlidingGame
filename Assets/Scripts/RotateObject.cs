262435339dba8400715385291e6c69e715e63eb3
mitsuru_yamaura <komayou326@gmail.com>
2020-12-20 13:08:04 +0900

    時間の経過に合わせて、ランダムな方向へ倒れる障害物のギミックを追加。 一定の距離にキャラが侵入すると出現する障害物のギミックを追加。

---
+++ Assets/Scripts/RotateObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class RotateObject : MonoBehaviour
+{
+    public float waitTime;
+
+
+    void Start()
+    {
+        StartCoroutine(Rotate(Random.Range(2.0f, 4.0f)));
+    }
+
+    private IEnumerator Rotate(float duration) {
+        yield return new WaitForSeconds(waitTime);
+
+        transform.DORotate(new Vector3(0, 0, RandomRotate()), duration).SetEase(Ease.Linear);
+    }
+
+    private float RandomRotate() {
+        float z = Random.Range(0, 2);
+        return z == 0 ? 70 : -70;
+    }
+}
