c6b76e5351f122b27a3a715164866f6c98ffe204
mitsuru_yamaura <komayou326@gmail.com>
2020-12-21 16:36:35 +0900

    FX‗MagicEffectアセット追加。DOTween追加。 得点用のオブジェクト設置。スクリプトから得点加算処理実装。

---
+++ Assets/Scripts/Circle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circle : MonoBehaviour
+{
+    public int point;
+
+    private void OnTriggerEnter(Collider col) {
+        if (col.gameObject.tag == "Player") {
+            Debug.Log(point);
+
+            if (col.gameObject.TryGetComponent(out PlayerController player)) {
+                player.AddScore(point);
+            }
+
+            Destroy(gameObject, 0.5f);
+        }
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private Text txtScore;
+
+    public void UpdateDisplayScore(int score) {
+        txtScore.text = score.ToString();
+    }
+}
