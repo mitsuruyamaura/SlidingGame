97b3c4e136299803cfdd86db6fc2f6694b96cc09
mitsuru_yamaura <komayou326@gmail.com>
2021-10-01 23:57:39 +0900

    ARCameraの追従処理の追加、位置を調整。実機デバッグ完了。

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField]
+    private GameObject cameraPos;
+
+    [SerializeField]
     private GameObject playerObj;
 
     private Vector3 offset;
 
     void Start() {
         // カメラと追従対象のゲームオブジェクトとの距離を補正値として取得
-        offset = transform.position - playerObj.transform.position;
+        offset = cameraPos.transform.position - playerObj.transform.position;
     }
 
 
@@ -20,7 +23,7 @@ public class CameraController : MonoBehaviour
         if (playerObj != null) {
 
             // カメラの位置を追従対象の位置 + 補正値にする
-            transform.position = playerObj.transform.position + offset;
+            cameraPos.transform.position = playerObj.transform.position + offset;
         }
     }
 }
